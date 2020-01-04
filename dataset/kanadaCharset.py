6c2a6c746f343969f04b71d0b38704a80fecad7c
asastiwa <asastiwa@ASASTIWA-IN.oradev.oraclecorp.com>
2020-01-04 22:35:15 +0530

    DataSet preparation

---
+++ dataset/kanadaCharset.py
@@ -0,0 +1,25 @@
+import io
+
+text_string= "ಅ ಆ ಇ ಈ ಉ ಊ ಋ ಌ ಎ ಏ ಐ ಒ ಓ ಔ ೠ"
+
+chars=['ಅ','ಆ','ಇ','ಈ','ಉ','ಊ','ಋ','ಌ','ಎ','ಏ','ಐ','ಒ','ಓ','ಔ','ೠ']
+
+with io.open("kanadaCharset.txt", "w", encoding="utf-8") as f:
+    for x in range(len(chars)):
+        f.write(chars[x])
+        f.write("\n")
+
+
+    for x in range(len(chars)):
+        for y in range(len(chars)):
+            f.write(chars[x]+chars[y])
+            f.write("\n")
+            
+
+'''
+    for x in range(len(chars)):
+        for y in range(len(chars)):
+            for z in range(len(chars)):
+                f.write(chars[x]+chars[y]+chars[z])
+                f.write("\n")
+'''
